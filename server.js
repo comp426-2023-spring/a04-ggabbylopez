898c1acebfe0fbf1b5611bb8c30cfa643e9c3ee3
ggabbylopez <gml0426@email.unc.edu>
2023-04-30 14:04:56 -0400

    reconfigure package

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import minimist from "minimist";
-import express from "express";
+import minimist from 'minimist';
+import express from 'express';
 import {rps,rpsls} from ".lib/rpsls.js"
 
 
